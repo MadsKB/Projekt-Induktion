c93835a018c37d6f085189d934fbf8400c1f17de
MadsKB <mads.krogh.b@gmail.com>
2022-05-10 10:11:14 +0200

    Split af data virker nu

--- Databehandling.py
+++ Databehandling.py
@@ -19,15 +19,33 @@ def divideBy(arrays,axis, start, end, num):
 
     delta = (end - start)/num
     divider = arrays[axis]
+
+    Empt = np.empty(len(divider))
+    Empt[:] = np.nan
+
     for i in range(0,num):
-        indexs = np.where(start+delta*i =< divider =<end+delta*i,range(len(divider)),np.empty(len(divider)))
-        indexs = indexs[~(numpy.isnan(indexs))]
+        print("Start",(start+delta*i))
+        print("End", (delta*(i+1)))
+        indexs = np.where(np.logical_and((start+delta*i) <= divider, divider < (delta*(i+1))),np.array(list(range(len(divider))),dtype = np.int64),Empt)
+        #print(indexs)
+        indexs = indexs[~(np.isnan(indexs))]
+        #print(indexs)
         out_array = []
         for array in arrays:
-            out_array +=np.array(array[indexs])
-        out_Arrays += [np.array(out_array)]
+            #print(array)
+            out_array += [array[indexs.astype(np.int64)]]
+        out_Arrays += [out_array]
+    print(out_Arrays)
+    return out_Arrays
 
-    return np.array(out_Arrays)
+"""
+Udregner chi^2 af fit (chi^2_min)
+"""
+
+def chi2_min(fit,x,y,parms,yerr):
+    return np.sum((y-fit(x,*parms))/yerr)
+
+
 
 """
 Objekt til at gemme på værdier som gælder for hele undersøgelsen, dvs. vægte af lod, hvad fps på kameraet er, osv.
@@ -66,7 +84,7 @@ class Maaling(diverseVærdier):
             self.m_lod = self.m_medium
         elif "tung" in file:
             self.m_lod = self.m_tung
-        print(self.m_lod)
+
         """
         Offset mellem magnetens, og loddets position; vi mangler måske noget logik her til at tage højde for for hvilket lod vi bruger
 
@@ -108,13 +126,14 @@ class Maaling(diverseVærdier):
         """
         Og Acceleration
         """
-        self.as = np.insert(np.diff(self.vs)/np.diff(self.ts),  0,0, axis = 0)
-        self.as_unc = np.zeros(len(self.as))
+
+        self.As = np.insert(np.diff(self.vs)/np.diff(self.ts),  0,0, axis = 0)
+        self.As_unc = np.zeros(len(self.As))
         """
         Og endeligt udregner vi størrelsen på bremsekraften
         """
 
-        self.Fs = self.as*(self.m_magnet+self.m_lod)+self.g*(self.m_lod-self.m_magnet)
+        self.Fs = self.As*(self.m_magnet+self.m_lod)+self.g*(self.m_lod-self.m_magnet)
         self.Fs_unc = np.zeros(len(self.Fs))
 
         return
@@ -131,12 +150,12 @@ class ledendeElement(diverseVærdier):
         self.ts = np.array([])
         self.ys = np.array([])
         self.vs = np.array([])
-        self.as = np.array([])
+        self.As = np.array([])
         self.Fs = np.array([])
 
         self.ys_unc = np.array([])
         self.vs_unc = np.array([])
-        self.as_unc = np.array([])
+        self.As_unc = np.array([])
         self.Fs_unc = np.array([])
 
         return
@@ -148,12 +167,12 @@ class ledendeElement(diverseVærdier):
         self.ts = np.append(self.ts,maaling.ts)
         self.ys = np.append(self.ys,maaling.ys)
         self.vs = np.append(self.vs,maaling.vs)
-        self.as = np.append(self.as,maaling.as)
+        self.As = np.append(self.As,maaling.As)
         self.Fs = np.append(self.Fs,maaling.Fs)
 
         self.ys_unc = np.append(self.ys_unc,maaling.ys_unc)
         self.vs_unc = np.append(self.vs_unc,maaling.vs_unc)
-        self.as_unc = np.append(self.as_unc,maaling.as_unc)
+        self.As_unc = np.append(self.As_unc,maaling.As_unc)
         self.Fs_unc = np.append(self.Fs_unc,maaling.Fs_unc)
 
 
@@ -163,20 +182,53 @@ class ledendeElement(diverseVærdier):
 
 """
 """
+class undersøgelse:
+    def __init__(self, dataFolder):
+        self.rør = ledendeElement()
+        self.Spole = ledendeElement()
+        self.SpoleLukket = ledendeElement()
 
-dataFolder = "."
+        self.rørFiles           = []
+        self.SpoleFiles         = []
+        self.SpoleLukketFiles   = []
+
+
+        allFiles = os.listdir(dataFolder)
+
+        for file in allFiles:
+
+            #problemer med file Spole, let, bund 1.txt (Nogle af y-værdierne i den manglet, dette fikset jeg ved at erstatte disse med 0)
+            if "Rør" in file:
+                self.rørFiles += [file]
+                self.rør.addData(dataFolder+file)
+            elif "Spole lukket" in file:
+                self.SpoleLukketFiles += [dataFolder+file]
+                self.SpoleLukket.addData(dataFolder+file)
+            elif "Spole" in file:
+                self.SpoleFiles += [dataFolder+file]
+                self.Spole.addData(dataFolder+file)
+
+
+
+        return
 
 
 
 
-file = "Rør, let, bund 1.txt"
+und = undersøgelse("./Datafiler/")
+
 
 fig, ax = plt.subplots(1,1)
 
-maaling = Maaling(file)
 
-ax.plot(maaling.ts, maaling.vs, marker = ".", ls = "None")
-ax.plot(maaling.ys, maaling.Fss, marker = ".", ls = "None")
+vedMunding = divideBy([und.rør.ys,und.rør.vs,und.rør.Fs],0,0,0.1,10)[0]
+#print(len(vedMunding))
+
+ax.plot(vedMunding[0],vedMunding[2],ls = "None", marker = ".")
+ax.set_ylabel(r"$F_{brems} \quad [N]$")
+ax.set_xlabel(r"$v \quad [m/s]$")
+ax.grid()
+
 
 
 plt.show()
